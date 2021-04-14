1b25c916f004f492d0db144796e0bfd0db70e461
mlmargaretli6@gmail.com <>
2021-04-13 23:40:45 -0700

    revisions

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
   noFill();
 }
 
-function draw(){
+function draw() {
   if (mouseIsPressed) {
     array.push([mouseX, mouseY]);
     drawAnimal();
@@ -15,39 +15,39 @@ function draw(){
 }
 
 function keyTyped() {
-    if (key === 'r') {  //red stroke
-      stroke(255, 0, 0);
-    }
-    if (key === 'g') {  //green stroke
-      stroke(0, 180, 0);
-    }
-    if (key === 'b') {  //blue stroke
-      stroke(0, 80, 235);
-    }
-    if (key === 's') {  //save image
-      saveCanvas('drawing', 'png');
-    }
+  if (key === 'r') { //red stroke
+    stroke(255, 0, 0);
+  }
+  if (key === 'g') { //green stroke
+    stroke(0, 180, 0);
+  }
+  if (key === 'b') { //blue stroke
+    stroke(0, 80, 235);
+  }
+  if (key === 's') { //save image
+    saveCanvas('drawing', 'png');
+  }
+}
+
+function drawAnimal() {
+
+
+  beginShape(); //draw image in curvilinear lines
+  //fill(150);
+  for (let i = 0; i < array.length; i++) {
+    curveVertex(array[i][0], array[i][1]);
+  }
+  endShape();
+
+  //display the same image at a smaller scale
+  push();
+  translate(0.5 * width, 0.5 * height);
+  beginShape();
+  scale(0.5);
+  for (let i = 0; i < array.length; i++) {
+    curveVertex(array[i][0], array[i][1]);
   }
 
-function drawAnimal(){
-
-
-      beginShape();    //draw image in curvilinear lines
-      //fill(150);
-      for (let i = 0; i < array.length; i++) {
-        curveVertex(array[i][0], array[i][1]);
-      }
-      endShape();
-
-      //display the same image at a smaller scale
-      push();
-      translate(0.5*width, 0.5*height);
-      beginShape();
-      scale(0.5);
-      for (let i = 0; i < array.length; i++) {
-      curveVertex(array[i][0], array[i][1]);
-      }
-
-      endShape();
-      pop();
+  endShape();
+  pop();
 }
